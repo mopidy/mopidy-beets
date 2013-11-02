2ce24f423ac1573c6c4b1a7a19672a84620042b5
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-11-02 23:16:33 +0100

    Remove old docstring

--- mopidy_beets/__init__.py
+++ mopidy_beets/__init__.py
@@ -1,27 +1,10 @@
 from __future__ import unicode_literals
 
 import os
+
 from mopidy import ext, config
 from mopidy.exceptions import ExtensionError
 
-
-__doc__ = """A extension for playing music from Beets.
-
-This extension handles URIs starting with ``beets:`` and enables you,
-to play music from Beets web service.
-
-See https://github.com/dz0ny/mopidy-beets/ for further instructions on
-using this extension.
-
-**Issues:**
-
-https://github.com/dz0ny/mopidy-beets/issues
-
-**Dependencies:**
-
-requests
-
-"""
 
 __version__ = '1.0.2'
 
