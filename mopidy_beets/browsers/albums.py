mopidy/mopidy-beets

commit 1e9ca370c15d74b8539e16aba8739f67b0aada3c

Port extension to Python3
Support for Python2 is dropped.

Closes: #26

mopidy_beets/browsers/albums.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 from mopidy import models
 
 from mopidy_beets.browsers import GenericBrowserBase
@@ -14,7 +12,7 @@ class AlbumsCategoryBrowser(GenericBrowserBase):
 
     def get_toplevel(self):
         keys = self.api.get_sorted_unique_album_attributes(self.field)
-        return [models.Ref.directory(name=unicode(key), uri=assemble_uri(
+        return [models.Ref.directory(name=key, uri=assemble_uri(
             self.ref.uri, id_value=key)) for key in keys]
 
     def get_directory(self, key):

mopidy_beets/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 import os
 
 from mopidy import config, ext

mopidy_beets/actor.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 import logging
 
 from mopidy import backend

mopidy_beets/browsers/__init__.py
@@ -1,6 +1,3 @@
-from __future__ import unicode_literals
-
-
 class GenericBrowserBase:
 
     def __init__(self, ref, api):

mopidy_beets/client.py
@@ -1,9 +1,9 @@
-from __future__ import unicode_literals
-
 import logging
 import re
 import time
-import urllib
+import urllib.error
+import urllib.parse
+import urllib.request
 
 from mopidy import httpclient
 
@@ -126,23 +126,16 @@ class BeetsRemoteClient(object):
         exact_query_list = []
 
         def quote_and_encode(text):
-            # utf-8 seems to be necessary for Python 2.7 and urllib.quote
-            if isinstance(text, unicode):
-                text = text.encode('utf-8')
-            elif isinstance(text, (int, float)):
+            if isinstance(text, (int, float)):
                 text = str(text)
             # Escape colons. The beets web API uses the colon to separate
             # field name and search term.
-            try:
-                text = text.replace(':', r'\:')
-            except UnicodeDecodeError:
-                # required for python2 only (see above 'unicode' condition)
-                text = text.decode('utf-8').replace(':', r'\:').encode("utf-8")
+            text = text.replace(':', r'\:')
             # quoting for the query string
-            return urllib.quote(text)
+            return urllib.parse.quote(text)
 
         for attribute in attributes:
-            if isinstance(attribute, basestring):
+            if isinstance(attribute, str):
                 query_parts.append(quote_and_encode(attribute))
                 exact_query_list.append((None, attribute))
             else:
@@ -155,7 +148,7 @@ class BeetsRemoteClient(object):
                 # Try to add a simple regex filter, if we look for a string.
                 # This will reduce the ressource consumption of the query on
                 # the server side (and for our 'exact' matching below).
-                if exact_text and isinstance(value, basestring):
+                if exact_text and isinstance(value, str):
                     regex_query = '^{}$'.format(re.escape(value))
                     beets_query = '{}::{}'.format(
                         quote_and_encode(key), quote_and_encode(regex_query))
@@ -185,7 +178,7 @@ class BeetsRemoteClient(object):
                     items = [item for item in items if value in item.values()]
                 else:
                     # filtering is necessary only for text based attributes
-                    if items and isinstance(items[0][key], basestring):
+                    if items and isinstance(items[0][key], str):
                         items = [item for item in items if item[key] == value]
         return items
 
@@ -248,7 +241,7 @@ class BeetsRemoteClient(object):
         # art. Thus we need to ask for it and check the status code.
         url = '{0}/album/{1}/art'.format(self.api_endpoint, album_id)
         try:
-            request = urllib.urlopen(url)
+            request = urllib.request.urlopen(url)
         except IOError:
             # DNS problem or similar
             return None

mopidy_beets/library.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 import logging
 import re
 

mopidy_beets/translator.py
@@ -1,7 +1,7 @@
-from __future__ import unicode_literals
-
 import logging
-import urllib
+import urllib.error
+import urllib.parse
+import urllib.request
 
 from mopidy.models import Album, Artist, Track
 
@@ -153,7 +153,7 @@ def parse_uri(uri, uri_prefix=None):
                         uri_prefix, uri)
             return None, None
     if id_string is not None:
-        id_value = urllib.unquote(id_string.encode('ascii')).decode('utf-8')
+        id_value = urllib.parse.unquote(id_string)
         # convert track and album IDs to int
         if last_path_token in ('track', 'album'):
             try:
@@ -173,7 +173,7 @@ def assemble_uri(*args, **kwargs):
         return base_path
     else:
         # convert numbers and other non-strings
-        if not isinstance(id_value, (str, unicode)):
+        if not isinstance(id_value, str):
             id_value = str(id_value)
-        id_string = urllib.quote(id_value.encode('utf-8'))
-        return '%s;%s' % (base_path, id_string)
+        id_string = urllib.parse.quote(id_value)
+        return "%s;%s" % (base_path, id_string)

setup.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 import re
 
 from setuptools import find_packages, setup
@@ -25,7 +23,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'setuptools',
-        'Mopidy >= 1.0',
+        'Mopidy >= 3.0.0a3',
         'Pykka >= 1.1',
         'requests >= 2.0.0',
     ],
@@ -39,7 +37,9 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Multimedia :: Sound/Audio :: Players',
     ],
 )

commit 0df77563750a24c99b58e515f4d6f3b0555585db

fix: allow browsing by album year
The listing was broken since commit 1e9ca370c15d.
Now we enforce the string conversion for browse item names again.

mopidy_beets/browsers/albums.py
@@ -13,7 +13,7 @@ class AlbumsCategoryBrowser(GenericBrowserBase):
         keys = self.api.get_sorted_unique_album_attributes(self.field)
         return [
             models.Ref.directory(
-                name=key, uri=assemble_uri(self.ref.uri, id_value=key)
+                name=str(key), uri=assemble_uri(self.ref.uri, id_value=key)
             )
             for key in keys
         ]